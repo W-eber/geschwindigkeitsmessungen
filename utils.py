25a8b6d4a086fb63912c5d23183120d00d8d5355
martinst06 <mstoyanov@adobe.com>
2024-10-27 22:29:32 +0100

    read_json-Funktion zum Laden von Daten aus JSON-Datei hinzugefügt

--- utils.py
+++ utils.py
@@ -2,7 +2,10 @@ import pandas as pd
 import json
 
 def read_json(file_path):
-    pass
+    # JSON-Datei einlesen
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    return data
 
 def process_data(data):
     pass
