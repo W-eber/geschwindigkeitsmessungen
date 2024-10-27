67403705699d01eafb60d36e1b30b63251d57af7
martinst06 <mstoyanov@adobe.com>
2024-10-27 22:28:46 +0100

    Grundstruktur von main.py und utils.py hinzugefügt

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from utils import read_json, process_data
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import json
+
+def read_json(file_path):
+    pass
+
+def process_data(data):
+    pass
